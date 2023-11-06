2c41dcdf1dda6bce04accb612337601f8fe0e725
amandajg <100507834+amandajg@users.noreply.github.com>
2023-11-05 21:13:19 -0700

    Changing count buttons again

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import CountButton from "./CountButton/CountButton"
 import SearchBar from "./SearchBar/SearchBar"
 export default function Page() {
     return <div>
-    <CountButton /><CountButton />
+    <CountButton /><CountButton2 />
     <hr />
     <a href="https://github.com/amandajg/reactjs">My github repo!</a>
     <hr />
