7207036f45ed7d43f291819c1bf6ffecac1b2c04
amandajg <100507834+amandajg@users.noreply.github.com>
2023-11-05 20:59:20 -0700

    Initial commit to repo

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+import CountButton from "./CountButton/CountButton"
+import SearchBar from "./SearchBar/SearchBar"
+export default function Page() {
+    return <div>
+    <CountButton /><CountButton />
+    <hr />
+    <a href="https://github.com/amandajg">My github repo!</a>
+    <hr />
+    <SearchBar />
+    </div>
+}
